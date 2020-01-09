54f78ad19ecf4721cf940c8ffa411e4d55b0c202
Ruslan <bullyboy131@gmail.com>
2020-01-09 14:29:08 +0300

    Update sum.js
    reworked HW1

--- javascript/1-carry-sum/sum.js
+++ javascript/1-carry-sum/sum.js
@@ -2,21 +2,16 @@ function sum(a) {
   let currentSum = a;
 
   function trueSum(b) {
-    if(!b) return currentSum;
+    if(typeof b !== 'number') return currentSum;
     currentSum += b;
     return trueSum;
   }
 
-  trueSum.toString = function() {
-    return currentSum;
-  };
-
   return trueSum;
 }
-let test = {};
-test.a = sum(undefined)() === undefined;
-test.b = sum(1)() === 1;
-test.c = sum(1)(2)(3)(4)()  === 10;
-test.d = sum(0)(5)(3)(8)(-2)() === 14;
-test.e = sum(1)(1)(1)(1)(1)(1)(1)() === 7;
-test;
+
+console.assert( sum(undefined)() === undefined , "sum(undefined)() === undefined");
+console.assert( sum(1)() === 1 , "sum(1)() === 1");
+console.assert( sum(1)(2)(3)(4)()  === 10 , "sum(1)(2)(3)(4)()  === 10");
+console.assert( sum(0)(5)(3)(8)(-2)() === 14 , "sum(0)(5)(3)(8)(-2)() === 14");
+console.assert( sum(1)(1)(1)(1)(1)(1)(1)() === 7 , "sum(1)(1)(1)(1)(1)(1)(1)() === 7");

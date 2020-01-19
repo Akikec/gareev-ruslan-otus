6d3e6e8ab77a7da6e7c4cc2f0d0bb60dadfcf49f
Ruslan <bullyboy131@gmail.com>
2020-01-19 20:31:10 +0300

    HW3 reworked

--- javascript/3-getPath/getPath.js
+++ javascript/3-getPath/getPath.js
@@ -1,25 +1,22 @@
 function getPath(argument) {
-
-    var nodeFunc = function(elemWithNode) {
-        let nodeResult = elemWithNode.nodeName;
-        if (elemWithNode.classList.length)
-        {
-            let selectorsList = (typeof(elemWithNode.className) === "string") ? elemWithNode.className : elemWithNode.classList.baseVal || "";
-            let selectors = selectorsList.split(" ").join(".");
-            if(selectors[0]  !== '.' && selectors.length > 1) selectors = "." + selectors;
-            nodeResult = nodeResult + selectors; 
-        }
-        return nodeResult;
-    };
-
-    let parentElement = argument.parentElement;
-    let result = nodeFunc(argument);
-    while(parentElement && parentElement.nodeName != "BODY")
+    let result = buildSelectors(argument);
+    const parentElement = argument.parentElement;
+    if(parentElement && parentElement.nodeName != "BODY")
     {
-        let nodeParentSelectors = nodeFunc(parentElement);
-        result = nodeParentSelectors + " > "+ result;
-        parentElement = parentElement.parentElement;
+        result = getPath(parentElement) + " > " + result;       
     }   
     console.log(result);
     return result;
 }
+
+function buildSelectors(elemWithNode) {
+    let nodeResult = elemWithNode.nodeName;
+    if (elemWithNode.classList.length)
+    {
+        let selectorsList = (typeof(elemWithNode.className) === "string") ? elemWithNode.className : elemWithNode.classList.baseVal || "";
+        let selectors = selectorsList.split(" ").join(".");
+        if(selectors[0]  !== '.' && selectors.length > 1) selectors = "." + selectors;
+        nodeResult = nodeResult + selectors; 
+    }
+    return nodeResult;
+};
